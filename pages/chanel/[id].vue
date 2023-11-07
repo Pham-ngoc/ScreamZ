95a0bcbac14c6fa0bc4ade073b0fb3aa0e1ba572
phamduyhuy2k3 <huypd.dev@gmail.com>
2023-11-07 15:54:52 +0700

    first message

---
+++ pages/chanel/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <ChannelBar :chanelId="useRoute().params.id"></ChannelBar>
+  <content-container></content-container>
+</template>
+<script setup>
+
+
+</script>
+
+<style scoped>
+
+</style>
