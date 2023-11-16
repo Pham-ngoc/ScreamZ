b7965240498b2197b5c8e897c4162adaa0a37572
khoa1242003 <doandangkhoa1242003@gmail.com>
2023-11-16 15:04:14 +0700

    abc

--- components/Modal/InviteConversation.vue
+++ components/Modal/InviteConversation.vue
@@ -6,10 +6,14 @@
   <!-- invite conversation  modal -->
     <div id="inviteConversationModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-[100000] hidden  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative w-full max-w-2xl max-h-full">
+
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <!-- Modal header -->
                 <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                        <path d="M16 0H4a2 2 0 0 0-2 2v1H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4.5a3 3 0 1 1 0 6 3 3 0 0 1 0-6ZM13.929 17H7.071a.5.5 0 0 1-.5-.5 3.935 3.935 0 1 1 7.858 0 .5.5 0 0 1-.5.5Z"/>
+                    </svg>
                     <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                         Invite friends to Conversation 1
                     </h3>
@@ -22,9 +26,15 @@
 
                 </div>
                 <!-- Modal body -->
-                <div class="p-6 space-y-6">
-                    <ul role="list" class="max-w-sm divide-y divide-gray-200 dark:divide-gray-700">
-                        <li class="p-6 space-y-6">
+
+                <div id="accordion-arrow-icon" data-accordion="open">
+                    <h2 id="accordion-arrow-icon-heading-1">
+                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-900 bg-gray-100 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-arrow-icon-body-1" aria-expanded="true" aria-controls="accordion-arrow-icon-body-1">
+                            <span>Looking for friends</span>
+                        </button>
+                    </h2>
+                    <div id="accordion-arrow-icon-body-1" aria-labelledby="accordion-arrow-icon-heading-1">
+                        <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                             <div class="flex items-center space-x-3 rtl:space-x-reverse">
                                 <div class="flex-shrink-0">
                                     <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Neil image">
@@ -37,36 +47,39 @@
                                         email@flowbite.com
                                     </p>
                                 </div>
-                                <span class="inline-flex items-center  text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
-                 <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-auto">Invite</button>
+                                <span class="inline-flex items-center  text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Invite</button>
             </span>
                             </div>
-                        </li>
-                        <li class="p-6 space-y-6">
                             <div class="flex items-center space-x-3 rtl:space-x-reverse">
                                 <div class="flex-shrink-0">
-                                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Neil image">
+                                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Neil image">
                                 </div>
                                 <div class="flex-1 min-w-0">
                                     <p class="text-sm font-semibold text-gray-900 truncate dark:text-white">
-                                        Bonnie Green
+                                        Neil Sims
                                     </p>
                                     <p class="text-sm text-gray-500 truncate dark:text-gray-400">
                                         email@flowbite.com
                                     </p>
                                 </div>
-                                <span class="inline-flex items-center  text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">
-                <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-auto">Invite</button>
+                                <span class="inline-flex items-center  text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Invite</button>
             </span>
                             </div>
-                        </li>
 
-                    </ul>
+                        </div>
+                    </div>
 
 
                 </div>
+
                 <!-- Modal footer -->
+
                 <div class="flex flex-col items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                        Or send them an invitation link
+                    </p>
                     <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-gray-100 dark:bg-black-800 dark:text-blue-400" style="color: black" role="alert">
                         <span class="font-medium">   https://conversation1.gg/bEMxueWC</span>
                         <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-auto">Copy</button>
