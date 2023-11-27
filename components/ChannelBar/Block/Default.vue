f5cf8cb41ec26e3cf1004e377a62b28301ef1669
phamduyhuy2k3 <huypd.dev@gmail.com>
2023-11-27 15:18:38 +0700

    Chat component

---
+++ components/ChannelBar/Block/Default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template >
+  <div class='channel-block p-3'>
+    <button type="button"  class="block w-full py-3 text-[10px]] text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+      Find or start a conversation
+    </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
